74e38b39b5741829d5c9e030f1d4ef4e9165addd
Alan Javier <87110072+AlanJSan@users.noreply.github.com>
2023-08-29 14:12:51 -0600

    Added bgs from other pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,9 +8,18 @@ module.exports = {
 		},
 		extend: {
 			backgroundImage: {
-				'mobile': 'url(/assets/home/background-home-mobile.jpg)',
+				'mobile': "url('/assets/home/background-home-mobile.jpg')",
 				'tablet': "url('/assets/home/background-home-tablet.jpg')",
-				'desktop': "url('/assets/home/background-home-desktop.jpg')"
+				'desktop': "url('/assets/home/background-home-desktop.jpg')",
+				'crew-mobile': 'url(/assets/crew/background-crew-mobile.jpg)',
+				'crew-tablet': 'url(/assets/crew/background-crew-tablet.jpg)',
+				'crew-desktop': 'url(/assets/crew/background-crew-desktop.jpg)',
+				'destination-mobile': 'url(/assets/destination/background-destination-mobile.jpg)',
+				'destination-tablet': 'url(/assets/destination/background-destination-tablet.jpg)',
+				'destination-desktop': 'url(/assets/destination/background-destination-desktop.jpg)',
+				'technology-mobile': 'url(/assets/technology/background-technology-mobile.jpg)',
+				'technology-tablet': 'url(/assets/technology/background-technology-tablet.jpg)',
+				'technology-desktop': 'url(/assets/technology/background-technology-desktop.jpg)',
 			  }
 		},
 	},
