12571ebbc95a5d305beac957c536cbf5e6c8fecd
Alan Javier <87110072+AlanJSan@users.noreply.github.com>
2023-11-13 13:43:05 -0600

    Add ViewTransitions, astro:page-swap event fix

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,11 +1,16 @@
-const menu = document.querySelector('.menu');
-document.querySelector(".hamburger").addEventListener("click", () => {
+events();
+
+document.addEventListener("astro:after-swap", () => { events() });
+
+function events() {
+  const menu = document.querySelector(".menu");
+  document.querySelector(".hamburger svg").addEventListener("click", () => {
     menu.classList.toggle("invisible");
     menu.classList.replace("opacity-0", "opacity-1");
-})
+  });
 
-document.querySelector(".close").addEventListener("click", () =>{
+  document.querySelector(".close").addEventListener("click", () => {
     menu.classList.replace("opacity-1", "opacity-0");
-    setTimeout(() => menu.classList.toggle("invisible"),150)    
-    
-})
+    setTimeout(() => menu.classList.toggle("invisible"), 150);
+  });
+}
